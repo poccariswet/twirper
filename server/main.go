d20d9aedc4a67a05d226ccd5a3c87b36c1684681
poccariswet <s1240231@gmail.com>
2019-03-06 13:14:50 +0900

    [ADD] server twiper

---
+++ server/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"context"
+	"net/http"
+
+	pb "github.com/poccariswet/twirper/rpc"
+)
+
+type helloworldServer struct{}
+
+func (s *helloworldServer) Hello(ctx context.Context, req *pb.HelloReq) (*pb.HelloResp, error) {
+	return &pb.HelloResp{Text: "helloooo, " + req.GetSubject()}, nil
+}
+
+func main() {
+	twirpHandler := pb.NewHelloWorldServer(&helloworldServer{}, nil)
+	mux := http.NewServeMux()
+	mux.Handle(pb.HelloWorldPathPrefix, twirpHandler)
+
+	http.ListenAndServe(":8080", mux)
+}
